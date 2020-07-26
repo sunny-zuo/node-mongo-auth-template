7ab9ff798c7fc3b13ce6a4dda4a0889a6ffc67c7
sunny-zuo <me@sunnyzuo.com>
2020-07-25 23:37:52 -0600

    feat: add bcrypt for password storage

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 
 app.use('/users', require('./users/users'));
--- server/users/users.js
+++ server/users/users.js
@@ -1,15 +1,22 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/authenticate', authenticate);
 
-function register(req, res, next) {
+function register(req, res) {
+    // check if user already exists
 
+    // register user
+    let hash = bcrypt.hashSync(req.body.password);
+    console.log(hash);
+    res.status(200).send('success');
 }
 
 function authenticate(req, res, next) {
-
+    // validate password
+    bcrypt.compareSync(req.body.password, '$2a$10$zSttFj9BFtMi38jWiJLbzeQly.lmdw2.dYeyNkKVG3kGbR4cW0yPq');
 }
 
 module.exports = router;
